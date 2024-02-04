417c977edac12c577e53c14db584172c4dd6123b
ninobrillo <81695245+ninobrillo@users.noreply.github.com>
2024-02-04 15:20:57 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+    var y = window.innerWidth;
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }else {
+      x.style.display = "block";
+    }
+}
+  
